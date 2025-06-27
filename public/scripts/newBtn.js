468fc2957a45c6756c0ae7d92403ba925d0f8a04
wal-des <asawasa@gmail.com>
2025-06-27 16:13:33 +0200

    Delete public directory

--- public/scripts/newBtn.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  //Knapp för att öppna formuläret
-  const newQuoteBtn = document.getElementById("newQuoteBtn");
-
-  newQuoteBtn.addEventListener("click", () => {
-    window.location.href = "/add"; // Omdirigera till sidan för nytt citat
-  });
-});
